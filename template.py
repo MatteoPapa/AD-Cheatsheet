982897e9dff248159a40299e61f80c02fb02ddfb
Matteo Papa <papa.1896945@studenti.uniroma1.it>
2025-04-11 13:10:14 +0200

    Updated Template

--- template.py
+++ template.py
@@ -5,29 +5,39 @@ import sys
 import random
 import string
 from pwn import *
+from hashlib import sha256, sha1
+from Crypto.Util.number import getPrime
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.number import inverse
+import random
 
 def generate(ln=32):
     return ''.join(random.choice(string.ascii_letters + string.digits) for i in range(ln))
 
 addr = sys.argv[1]
 
+team_id= addr.split(".")[2]
 
-# CHANGE THIS -v
-PORT = 1337
-SERVICE = 'cc_market'
+PORT = 5555
+SERVICE = 'CC-Manager'
 
 URL = 'http://' + addr + ':' + str(PORT)
-FLAGID_URL = 'http://10.10.0.1:8081/flagIds?service={}&team={}'.format(SERVICE, addr)
+FLAGID_URL = 'http://10.10.0.1:8081/flagIds?service={}&team={}'.format(SERVICE,team_id)
 flag_ids = json.loads(requests.get(FLAGID_URL).text)
-flag_ids = flag_ids[SERVICE][addr]
+flag_ids = flag_ids[SERVICE][team_id]
 
 
-for flag_id in flag_ids:
+for key, info in flag_ids.items():
+    #Flag Ids Values:
+    username = info['username']
+    password = info['password']
 
-    s = requests.Session()
+    #If you need random shit:
     usr = generate()
     psw = generate()
-    a = s.post(URL + '/register', data={'username': usr, 'password': psw, 'secret': 'suca', 'category': 'osint', 'premium': '1'})
 
-    a = s.get(URL + '/profile/' + flag_id)
+    s = requests.Session()
+
+    a = s.post(URL + '/register', data={'username': usr, 'password': psw, 'secret': 'suca', 'category': 'osint', 'premium': '1'})
     print(a.text, flush=True)
